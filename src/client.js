09556579d937cc790e3466e33b7356aaf41fdafc
jonny <lutz.kwauka@payango.com>
2017-01-29 04:39:46 +0100

    client entry

---
+++ src/client.js
@@ -0,0 +1,75 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FastClick from 'fastclick';
+import { Provider } from 'react-redux';
+import { useScroll } from 'react-router-scroll';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { ReduxAsyncConnect } from 'redux-connect';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, browserHistory, applyRouterMiddleware, match } from 'react-router';
+
+import getRoutes from './routes';
+import ApiClient from './helpers/ApiClient';
+import createStore from './redux/create';
+
+const client = new ApiClient();
+const store = createStore(browserHistory, client, window.__data);
+const syncHistory = syncHistoryWithStore(browserHistory, store);
+const rootDomNode = document.getElementById('content');
+const routerRoutes = getRoutes(store);
+const asyncConnectRender = applyRouterMiddleware(useScroll());
+
+const renderRouter = props =>
+  <ReduxAsyncConnect
+    { ...props }
+    helpers={{ client }}
+    filter={ item => !item.deferred }
+    render={ asyncConnectRender } />;
+
+const render = (routes, renderProps) => {
+  ReactDOM.render(
+    <Provider store={store} key="provider">
+      <Router
+        render={ renderRouter }
+        history={ syncHistory }
+        { ...renderProps }>
+        { routes }
+      </Router>
+    </Provider>,
+    rootDomNode
+  );
+};
+
+const renderDevStuff = () => {
+  if (__DEVELOPMENT__) {
+    window.React = React; // enable debugger
+
+    if (!rootDomNode || !rootDomNode.firstChild || !rootDomNode.firstChild.attributes || !rootDomNode.firstChild.attributes['data-react-checksum']) {
+      console.error('Server-side React render was discarded. Make sure that your initial render does not contain any client-side code.');
+    }
+  }
+
+  if (__DEVTOOLS__) {
+    const DevTools = require('./containers/DevTools/DevTools').default;
+    const devToolsDest = document.createElement('div');
+    window.document.body.insertBefore(devToolsDest, null);
+    ReactDOM.render(
+      <Provider store={store} key="provider">
+        <DevTools />
+      </Provider>,
+      devToolsDest
+    );
+  }
+};
+
+injectTapEventPlugin();
+FastClick.attach(document.body);
+
+match(
+  { history: syncHistory, routes: routerRoutes },
+  (error, redirectLocation, renderProps) => {
+    render(routerRoutes, renderProps);
+    renderDevStuff();
+  }
+);
