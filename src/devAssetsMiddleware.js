f97d59cb9634d38eb1cb5a65f4b5f7076c5a7e3e
jonny <lutz.kwauka@payango.com>
2017-02-03 14:34:45 +0100

    check first

--- src/devAssetsMiddleware.js
+++ src/devAssetsMiddleware.js
@@ -1,20 +1,22 @@
 const devAssetsMiddleware = (getDevAssets) => {
   return (req, res, next) => { // eslint-disable-line
+    if (!global.__DEVELOPMENT__) {
+      return next();
+    }
     if (req.path === '/favicon.ico') {
       return next();
     }
-    if (global.__DEVELOPMENT__) {
-      // in dev we refresh assets.json on every request
-      const devAssets = getDevAssets();
-      if (!devAssets) {
-        return res.status(500).send('no assets found for server render, make sure ./webpack-assets.json exists and refresh');
-        // return;
-      }
-      devAssets.then((assets) => {
-        req.devAssets = assets;
-        return next();
-      });
-    } else { return next(); }
+    // in dev we refresh assets.json on every request
+    const devAssets = getDevAssets();
+    if (!devAssets) {
+      return res.status(500).send('no assets found for server render, make sure ./webpack-assets.json exists and refresh');
+      // return;
+    }
+    devAssets.then((assets) => {
+      req.devAssets = assets;
+      return next();
+    });
+    
   };
 };
 
