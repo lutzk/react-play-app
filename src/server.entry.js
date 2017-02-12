171b157abaa4223bf49b4e25ec5371b6f475b89a
jonny <lutz.kwauka@gmail.com>
2017-02-12 23:12:15 +0100

    start direct in dev (no assetsPromise)

--- src/server.entry.js
+++ src/server.entry.js
@@ -5,9 +5,18 @@ global.__SERVER__ = true;
 global.__DISABLE_SSR__ = false; // <----- DISABLES SERVER SIDE RENDERING FOR ERROR DEBUGGING
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 
-const assetsPromise = getAssets();
-assetsPromise.then((assets) => {
-  import('./server').then((server) => {
-    return server.default(formatAssets(assets));
+const startServer = () => {
+  if (global.__DEVELOPMENT__) {
+    return import('./server').then((server) => {
+      return server.default({});
+    }).catch(e => console.log(e));
+  }
+
+  return getAssets().then((assets) => {
+    import('./server').then((server) => {
+      return server.default(formatAssets(assets));
+    });
   });
-});
+};
+
+startServer();
