a0fe86be0f2667bb8db895a299febf47e8c9ad62
jonny <lutz.kwauka@gmail.com>
2017-02-14 00:12:27 +0100

    home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component /* , PropTypes */ } from 'react';
 import { Link } from 'react-router';
+import './home.sass';
 
 export default class Home extends Component {
 
   render() {
 
     return (
-      <div>
+      <div className="home">
         <h1>home&nbsp;<Link to="/">login</Link>&nbsp;<Link to="/info">info</Link></h1>
       </div>);
   }
