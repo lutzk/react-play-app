11ab62c38bdafe79ed41bc7b31a592c7a43c0db5
jonny <lutz.kwauka@gmail.com>
2017-02-16 21:23:31 +0100

    document some errors

--- src/routes.js
+++ src/routes.js
@@ -9,17 +9,27 @@ import PATHS from './pathsConfig';
 
 export default function () {
 
-  const getHome = () => {
-    return import('./containers/Home/Home').then((m) => { return m.default; });
-  };
+  /*
+  const _getComponent = (path) => {
 
-  const getInfo = () => {
-    return import('./containers/Info/Info').then((m) => { return m.default; });
-  };
+    // with string concat:
+    //   on navigating there:
+    //     Uncaught Error: Cannot find module './containers/Home/Home'.
+    // code:
+    // const _path = './containers/' + path;// eslint-disable-line
+    // return () => import(_path).then((m) => { return m.default; });// eslint-disable-line
 
-  const getNotFound = () => {
-    return import('./containers/NotFound/NotFound').then((m) => { return m.default; });
+    // on compilation:
+    //   WARNING in ./src async ^.*$
+    //   Module not found: Error: [CaseSensitivePathsPlugin] `/Users/jonny/Desktop/do/src/containers/Home/home.js` does not match the corresponding path on disk `Home.js`.
+    // code:
+    // const _path = `./containers/${path}`;
+    // return () => import(`${_path}`).then((m) => { return m.default; });
   };
+  */
+
+  // const _getHome = _getComponent('Home/Home');
+
   const getHome = () => import('./containers/Home/Home').then((m) => { return m.default; });
   const getInfo = () => import('./containers/Info/Info').then((m) => { return m.default; });
   const getNotFound = () => import('./containers/NotFound/NotFound').then((m) => { return m.default; });
