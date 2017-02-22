e63af09989f67eac641b8a8b452d64706ee7ebfe
jonny <lutz.kwauka@gmail.com>
2017-02-22 17:22:30 +0100

    refresh eslint

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,11 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import './Login.sass';
 
-@connect( state =>
-  ({ app: state.app })
-)
 export default class Login extends Component {
 
   static propTypes = {
