bf257cacdcd479b578a58b78c3b80af60d0a5a61
jonny <lutz.kwauka@gmail.com>
2017-02-23 03:46:47 +0100

    hook loadingBar in redux clientMiddleware

--- src/redux/middleware/clientMiddleware.js
+++ src/redux/middleware/clientMiddleware.js
@@ -1,8 +1,14 @@
-// import CALL_HISTORY_METHOD from 'react-router-redux';
-// import { LOCATION_CHANGE } from 'react-router-redux';
+import { LOCATION_CHANGE } from 'react-router-redux';
+import { startLoading, endLoading } from '../modules/pageLoadBar';
 
-// import { beginGlobalLoad, endGlobalLoad } from 'redux-connect/lib/store';// eslint-disable-line
-import { LOADING, endLoading } from '../modules/pageLoadBar';
+// import CALL_HISTORY_METHOD from 'react-router-redux';
+// import { beginGlobalLoad, endGlobalLoad } from 'redux-connect/lib/store';
+
+// const REDUX_ASYNC_LOAD = '@redux-conn/LOAD';
+// const REDUX_ASYNC_LOAD_SUCCESS = '@redux-conn/LOAD_SUCCESS';
+// const REDUX_ASYNC_LOAD_FAIL = '@redux-conn/LOAD_FAIL';
+// const REDUX_GLOBAL_LOAD_BEGIN = @redux-conn/GLOBAL_LOAD_BEGIN;
+// const REDUX_GLOBAL_LOAD_END = @redux-conn/GLOBAL_LOAD_END;
 
 // const REDUX_GLOBAL_LOAD_BEGIN = beginGlobalLoad.toString();
 // const REDUX_GLOBAL_LOAD_END = endGlobalLoad.toString();
@@ -15,14 +21,18 @@ export default function clientMiddleware(client) {
       }
 
       // if (action.type === REDUX_GLOBAL_LOAD_BEGIN) {
-      //   dispatch({ type: LOADING });
-      // }
-
-      // if (action.type === REDUX_GLOBAL_LOAD_END) {
-      //   dispatch({ type: END_LOADING });
+      //   dispatch(startLoading());
       // }
 
       const { promise, types, ...rest } = action;
+
+      if (action.type === LOCATION_CHANGE && action.payload.pathname !== '/' && action.payload.key !== undefined) {
+        dispatch(startLoading());
+      }
+
+      if (action.type === '@redux-conn/END_GLOBAL_LOAD') {
+        dispatch(endLoading());
+      }
 
       if (!promise) {
         return next(action);
@@ -30,8 +40,9 @@ export default function clientMiddleware(client) {
 
       const [REQUEST, SUCCESS, FAILURE] = types;
 
+      dispatch(startLoading());
       next({ ...rest, type: REQUEST });
-      dispatch({ type: LOADING });
+
       return promise(client).then(
 
         (result) => {
@@ -49,6 +60,7 @@ export default function clientMiddleware(client) {
         }
       )
       .catch((error) => {
+        dispatch(endLoading(true));
         console.error('MIDDLEWARE ERROR:', error);
         next({ ...rest, error, type: FAILURE });
       });
