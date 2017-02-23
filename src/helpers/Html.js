332b7989277930f5a93fddcd3392cc9d6acb71c4
jonny <lutz.kwauka@gmail.com>
2017-02-23 23:27:04 +0100

    stateless component

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/server';
 import serialize from 'serialize-javascript';
 
@@ -11,56 +11,70 @@ import serialize from 'serialize-javascript';
  * HTML doctype declaration, which is added to the rendered output
  * by the server.js file.
  */
-export default class Html extends Component {
-  static propTypes = {
-    assets: PropTypes.object,
-    component: PropTypes.node,
-    store: PropTypes.object
-  }
 
-  render() {
-    const { assets, component, store } = this.props;
-    const content = component ? ReactDOM.renderToString(component) : '';
-    const windowData = { __html: `window.__data=${serialize(store.getState())};` };
-    const htmlContent = { __html: content };
+const Html = (_props) => {
 
-    const css = Object.keys(assets.styles).map((style, key) => {
-      if (assets.styles[style]) {
-        return (
-          <link
-            key={key}
-            rel="stylesheet"
-            href={assets.styles[style]}
-            type="text/css"
-            media="screen, projection"
-            charSet="UTF-8" />);
-      }
-      return null;
-    });
+  /*
+  * _props:
+  *   store: object
+  *   assets: object
+  *   component: node
+  */
 
-    const js = Object.keys(assets.javascript).map((script, key) => {
-      if (assets.javascript[script]) {
-        return (
-          <script
-            key={key}
-            charSet="UTF-8"
-            src={assets.javascript[script]} />
-        );
-      }
-      return null;
-    });
+  const { assets, component, store } = _props;
+  const content = component ? ReactDOM.renderToString(component) : '';
+  const windowData = { __html: `window.__data=${serialize(store.getState())};` };
 
-    return (
-      <html>
-        <head>
-          {css}
-        </head>
-        <body>
-          <div id="content" className="content" dangerouslySetInnerHTML={htmlContent} />
-          <script dangerouslySetInnerHTML={windowData} charSet="UTF-8" />
-          {js}
-        </body>
-      </html>
-    );
-  }
-}
+  const windowDataScript = (
+    <script
+      charSet="UTF-8"
+      dangerouslySetInnerHTML={windowData} />);
+
+  const htmlContent = (
+    <div
+      id="content"
+      className="content"
+      dangerouslySetInnerHTML={{ __html: content }} />);
+
+  const css = Object.keys(assets.styles).map((style, key) => {
+    if (assets.styles[style]) {
+      return (
+        <link
+          key={key}
+          rel="stylesheet"
+          href={assets.styles[style]}
+          type="text/css"
+          media="screen, projection"
+          charSet="UTF-8" />);
+    }
+    return null;
+  });
+
+  const js = Object.keys(assets.javascript).map((script, key) => {
+    if (assets.javascript[script]) {
+      return (
+        <script
+          key={key}
+          charSet="UTF-8"
+          src={assets.javascript[script]} />);
+    }
+    return null;
+  });
+
+  const html = (
+    <html>
+      <head>
+        {css}
+      </head>
+      <body>
+        {htmlContent}
+        {windowDataScript}
+        {js}
+      </body>
+    </html>);
+
+  return html;
+};
+
+export default Html;
+
