a015921d472279b758e0ab069e4c61b2cea1e551
jonny <lutz.kwauka@gmail.com>
2017-03-01 00:06:27 +0100

    split express server and react render logic

---
+++ src/server/middleware/renderApp.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactDOM from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { match, createMemoryHistory } from 'react-router';
+import { ReduxAsyncConnect, loadOnServer } from 'redux-connect';
+import { ApiClient, Html, logJSON } from '../../helpers';
+import getRoutes from '../../routes';
+import createStore from '../../redux/create';
+import { timer } from '../../helpers/logTiming';
+
+const renderApp = serverAssets => (req, res, next) => {
+
+  const startTime = timer.start();
+
+  let assets = serverAssets;
+  const client = new ApiClient(req);
+  const history = createMemoryHistory(req.originalUrl);
+  const store = createStore(history, client);
+  const routes = getRoutes(store);
+  const doctype = '<!doctype html>\n';
+
+  const renderHtml = (_store, _assets, _component) =>
+    ReactDOM.renderToString(
+      <Html
+        store={_store}
+        assets={_assets}
+        component={_component} />);
+
+  const hydrateOnClient = _assets =>
+    res.send(
+      `${doctype}${ReactDOM.renderToString(
+        <Html store={store} assets={_assets} />)}`
+    );
+
+  // in dev
+  if (global.__DEVELOPMENT__ && res.locals.devAssets) {
+    assets = res.locals.devAssets;
+  }
+
+  if (global.__DISABLE_SSR__) {
+    hydrateOnClient(assets);
+    return;
+  }
+
+  match(
+    { history, routes, location: req.originalUrl },
+    (error, redirectLocation, renderProps) => {
+      if (error) {
+        logJSON(error, 'error');
+        res.status(500);
+        hydrateOnClient(assets);
+        next();
+
+      } else if (redirectLocation) {
+        res.redirect(`${redirectLocation.pathname}${redirectLocation.search}`);
+        next();
+
+      } else if (renderProps) {
+
+        loadOnServer({ ...renderProps, store, helpers: { client } })
+        .then(() => {
+          const component = (
+            <Provider store={store} key="appProvider">
+              <ReduxAsyncConnect { ...renderProps } />
+            </Provider>
+          );
+          const html = `${doctype}${renderHtml(store, assets, component)}`;
+          res.status(200);
+          res.send(html);
+          timer.stop(startTime, 'renderApp');
+          next();
+        })
+        .catch((err) => {
+          logJSON({ catchError: err }, 'error');
+          next();
+        });
+      } else {
+        res.status(404).send('Not found');
+        next();
+      }
+    });
+};
+
+export default renderApp;
--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,12 @@
-import Express from 'express';
 import cors from 'cors';
 import http from 'http';
 import path from 'path';
-import React from 'react';
-import ReactDOM from 'react-dom/server';
+import Express from 'express';
 import compression from 'compression';
-import { Provider } from 'react-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { match, createMemoryHistory } from 'react-router';
-import { ReduxAsyncConnect, loadOnServer } from 'redux-connect';
-import { ApiClient, Html, logJSON } from '../helpers';
+import { logJSON } from '../helpers';
 import appConfig from '../config/appConfig';
-import getRoutes from '../routes';
-import createStore from '../redux/create';
+import renderApp from './middleware/renderApp';
 import devAssetsMiddleware from './middleware/devAssetsMiddleware';
 import { loadDevRoverManifest } from './middleware/loadDevRoverManifest';
 import { faviconReqKiller } from './middleware/faviconReqKiller';
@@ -21,7 +15,6 @@ injectTapEventPlugin();
 
 const app = new Express();
 const server = new http.Server(app);
-const doctype = '<!doctype html>\n';
 const staticDir = path.join(process.cwd(), './static');
 const corsConfig = {
   // origin: 'https://api.nasa.gov/',
@@ -29,80 +22,22 @@ const corsConfig = {
   methods: 'GET',
 };
 
-const startServer = (assets) => {
-
-  let assetsObj = assets;
+const startServer = (serverAssets = {}) => {
 
   if (global.__DEVELOPMENT__) {
-    app.use(devAssetsMiddleware());
-    app.use(loadDevRoverManifest());
+    app
+      .use(faviconReqKiller())
+      .use(devAssetsMiddleware())
+      .use(loadDevRoverManifest());
   }
 
   app
-    .use(cors(corsConfig))
-    .use(compression())
     .use(faviconReqKiller())
+    .use(loadDevRoverManifest())
+    .use(compression())
     .use(Express.static(staticDir))
-    .use((req, res) => {
-      const client = new ApiClient(req);
-      const history = createMemoryHistory(req.originalUrl);
-      const store = createStore(history, client);
-      const routes = getRoutes(store);
-
-      const renderHtml = (_store, _assets, _component) =>
-        ReactDOM.renderToString(
-          <Html
-            store={_store}
-            assets={_assets}
-            component={_component} />);
-
-      const hydrateOnClient = _assets =>
-        res.send(
-          `${doctype}${ReactDOM.renderToString(
-            <Html store={store} assets={_assets} />)}`
-        );
-
-      // in dev
-      if (global.__DEVELOPMENT__ && res.locals.devAssets) {
-        assetsObj = res.locals.devAssets;
-      }
-
-      if (global.__DISABLE_SSR__) {
-        hydrateOnClient(assetsObj);
-        return;
-      }
-
-      match(
-        { history, routes, location: req.originalUrl },
-        (error, redirectLocation, renderProps) => {
-          if (error) {
-            logJSON(error, 'error');
-            res.status(500);
-            hydrateOnClient(assetsObj);
-
-          } else if (redirectLocation) {
-            res.redirect(`${redirectLocation.pathname}${redirectLocation.search}`);
-
-          } else if (renderProps) {
-
-            loadOnServer({ ...renderProps, store, helpers: { client } })
-            .then(() => {
-              const component = (
-                <Provider store={store} key="appProvider">
-                  <ReduxAsyncConnect { ...renderProps } />
-                </Provider>
-              );
-              const html = `${doctype}${renderHtml(store, assetsObj, component)}`;
-              res.status(200);
-              res.send(html);
-            }).catch((err) => {
-              logJSON({ catchError: err }, 'error');
-            });
-          } else {
-            res.status(404).send('Not found');
-          }
-        });
-    });
+    .use(cors(corsConfig))
+    .use(renderApp(serverAssets));
 
   if (appConfig.port) {
     server.listen(appConfig.port, (err) => {
