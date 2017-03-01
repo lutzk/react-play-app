8f8f5463964be585aea29e860c2f0816656b318d
jonny <lutz.kwauka@gmail.com>
2017-03-01 22:00:05 +0100

    use built in promise from superagent, clean up

--- src/helpers/ApiClient.js
+++ src/helpers/ApiClient.js
@@ -3,47 +3,32 @@ import superagent from 'superagent';
 
 const methods = ['get', 'post', 'put', 'patch', 'del'];
 
-/*
- * This silly underscore is here to avoid a mysterious "ReferenceError: ApiClient is not defined" error.
- * See Issue #14. https://github.com/erikras/react-redux-universal-hot-example/issues/14
- *
- * Remove it at your own risk.
- */
-
- // use superagent promise
- // not tested
-class _ApiClient1 {
-  constructor(/* incomingReq */) {
+class ApiClient {
+  constructor(incomingReq) {
     methods.forEach(method =>// eslint-disable-line
-      this[method] = (path /* , {  params, data  }  = {} */) =>// eslint-disable-line
-        superagent[method](path)
-        // return superagent[method](path)
+      this[method] = (path, {  params, data  }  = {}) => {// eslint-disable-line
+        const request = superagent[method](path);
         // if (cookie && cookie.load('loginResult')) {
         //   request.set(config.authTokenKey, cookie.load('loginResult'));
         // }
-        // if (params) {
-        //   request.query(params);
-        // }
-        // if (global.__SERVER__ && incomingReq.get('cookie')) {
-        //   request.set('cookie', incomingReq.get('cookie'));
-        // }
-        // if (data) {
-        //   request.send(data);
-        // }
-        .on('error', e => console.log('error: ', e, JSON.stringify(e, 0, 2)))
-        .then((success, error) => {
+        if (params) {
+          request.query(params);
+        }
+        if (global.__SERVER__ && incomingReq.get('cookie')) {
+          request.set('cookie', incomingReq.get('cookie'));
+        }
+        if (data) {
+          request.send(data);
+        }
+        return request.then((success, error) => {
           if (error) {
-            console.error(JSON.stringify(error, 0, 2));
-            return error;
+            return success || error;
           }
 
           return success.body;
-        })
-        .catch(e => e)
-    );
+        });
+      });
   }
 }
 
-const Api2 = _ApiClient1;
-
-export default Api2;
+export default ApiClient;
