1e9b099bdb0bce89ea30fe0ce0e966398045d83e
jonny <lutz.kwauka@gmail.com>
2017-03-07 00:26:23 +0100

    update timer
    stop takes a start and a optional label

--- src/helpers/logTiming.js
+++ src/helpers/logTiming.js
@@ -1,9 +1,9 @@
 export const timer = { // eslint-disable-line
-  start: () => process.hrtime(),
-  stop: (_start, label = false) => {
-    const diff = process.hrtime(_start);
+  start: (label = false) => ({ label, time: process.hrtime() }),
+  stop: (_start) => {
+    const diff = process.hrtime(_start.time);
     const nanosecs = (diff[0] * 1e9) + diff[1];
     const secs = nanosecs / 1000000000;
-    console.log(`${label || 'operation'} took: ${secs} secs`);
+    console.log(`${_start.label || 'operation'} took: ${secs} secs`);
   },
 };
