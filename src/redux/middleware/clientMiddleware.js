62cfdac5034711ffa0a648e8dfe8a6c4d72b5143
jonny <lutz.kwauka@gmail.com>
2017-03-16 22:07:04 +0100

    use async await for client middleware

--- src/redux/middleware/clientMiddleware.js
+++ src/redux/middleware/clientMiddleware.js
@@ -1,66 +1,44 @@
 import { LOCATION_CHANGE } from 'react-router-redux';
 import { startLoading, endLoading } from '../modules/pageLoadBar';
 
-// import CALL_HISTORY_METHOD from 'react-router-redux';
-// import { beginGlobalLoad, endGlobalLoad } from 'redux-connect/lib/store';
+const clientMiddleware = client => ({ dispatch, getState }) => next => async (action) => {
 
-// const REDUX_ASYNC_LOAD = '@redux-conn/LOAD';
-// const REDUX_ASYNC_LOAD_SUCCESS = '@redux-conn/LOAD_SUCCESS';
-// const REDUX_ASYNC_LOAD_FAIL = '@redux-conn/LOAD_FAIL';
-// const REDUX_GLOBAL_LOAD_BEGIN = @redux-conn/GLOBAL_LOAD_BEGIN;
-// const REDUX_GLOBAL_LOAD_END = @redux-conn/GLOBAL_LOAD_END;
+  if (typeof action === 'function') {
+    return action(dispatch, getState);
+  }
 
-// const REDUX_GLOBAL_LOAD_BEGIN = beginGlobalLoad.toString();
-// const REDUX_GLOBAL_LOAD_END = endGlobalLoad.toString();
-// const clientMiddleware = client =>
-// ({ dispatch, getState }) => next => action => {
-// export default clientMiddleware
+  const { promise, types, ...rest } = action;
 
-export default function clientMiddleware(client) {
-  return ({ dispatch, getState }) => next => (action) => {
-    if (typeof action === 'function') {
-      return action(dispatch, getState);
-    }
-
-    // if (action.type === REDUX_GLOBAL_LOAD_BEGIN) {
-    //   dispatch(startLoading());
-    // }
-
-    const { promise, types, ...rest } = action;
-
-    if (action.type === LOCATION_CHANGE && action.payload.pathname !== '/' && action.payload.key !== undefined) {
-      dispatch(startLoading());
-    }
-
-    if (action.type === '@redux-conn/END_GLOBAL_LOAD') {
-      dispatch(endLoading());
-    }
-
-    if (!promise) {
-      return next(action);
-    }
-
-    const [REQUEST, SUCCESS, FAILURE] = types;
-
+  if (action.type === LOCATION_CHANGE && action.payload.pathname !== '/' && action.payload.key !== undefined) {
     dispatch(startLoading());
-    next({ ...rest, type: REQUEST });
+  }
 
-    return promise(client).then(
+  if (action.type === '@redux-conn/END_GLOBAL_LOAD') {
+    dispatch(endLoading());
+  }
 
-      (result) => {
-        dispatch(endLoading());
-        return next({ ...rest, result, type: SUCCESS });
-      },
+  if (!promise) {
+    return next(action);
+  }
 
-      (error) => {
-        dispatch(endLoading(true));
-        return next({ ...rest, error, type: FAILURE });
-      }
-    )
-    .catch((error) => {
-      dispatch(endLoading(true));
-      console.error('MIDDLEWARE ERROR:', error);
-      next({ ...rest, error, type: FAILURE });
-    });
-  };
-}
+  const [REQUEST, SUCCESS, FAILURE] = types;
+
+  dispatch(startLoading());
+  next({ ...rest, type: REQUEST });
+
+  let result;
+
+  try {
+    result = await promise(client);
+    dispatch(endLoading());
+
+  } catch (error) {
+    result = error;
+    dispatch(endLoading(true));
+    return next({ ...rest, error, type: FAILURE });
+  }
+
+  return next({ ...rest, result, type: SUCCESS });
+};
+
+export default clientMiddleware;
