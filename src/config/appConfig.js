656c919a017c21a2f922765e95cf049ea90ec8ec
jonny <lutz.kwauka@gmail.com>
2017-04-20 06:02:21 +0200

    comment out

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,10 +1,10 @@
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3010;
 // const protocol = process.env.SSL ? 'http://' : 'https://';
-const apiHost = process.env.API_HOST || 'localhost';
-const apiPort = process.env.API_PORT || 3040;
+// const apiHost = process.env.API_HOST || 'localhost';
+// const apiPort = process.env.API_PORT || 3040;
 const apiBase = process.env.API_BASE || '/api/v1';
-const apiPath = `http://${apiHost}:${apiPort}${apiBase}`;
+// const apiPath = `http://${apiHost}:${apiPort}${apiBase}`;
 const apiSocket = '/tmp/api.sock';
 const authTokenKey = 'x-authentication';
 const ssrAssetsRoute = process.env.SSR_ASSETS_ROUTE;
@@ -14,10 +14,7 @@ const devAssetServerPath = `http://${host}:${devAssetServerPort}${ssrAssetsRoute
 export {
   host,
   port,
-  apiHost,
-  apiPort,
   apiBase,
-  apiPath,
   apiSocket,
   authTokenKey,
   ssrAssetsRoute,
