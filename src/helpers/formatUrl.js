1e8e4a17d84973304f6fcd64c1ff824dc8c2140b
jonny <lutz.kwauka@gmail.com>
2017-04-20 05:58:18 +0200

    adjust path

--- src/helpers/formatUrl.js
+++ src/helpers/formatUrl.js
@@ -1,4 +1,4 @@
-import { apiBase, apiPath } from '../config/appConfig';// eslint-disable-line
+import { host, port, apiBase } from '../config/appConfig';
 
 const lead = '/';
 
@@ -9,7 +9,7 @@ const prefix = path =>
 
 const formatUrl = path =>
   __SERVER__ ?
-    `localhost:3010/api/v1${prefix(path)}`
+    `${host}:${port}${apiBase}${prefix(path)}`
     : apiBase + prefix(path);
 
 export default formatUrl;
