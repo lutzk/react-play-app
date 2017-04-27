c04524cdbce674702690b437ca8fdc307290cedc
jonny <lutz.kwauka@gmail.com>
2017-04-27 23:19:03 +0200

    remove console.log

--- src/appServer/middleware/getCouchDocs.js
+++ src/appServer/middleware/getCouchDocs.js
@@ -2,8 +2,7 @@ import { ApiClient } from '../../helpers/ApiClient';
 import { asyncWrap as aw } from '../../helpers/utils';
 
 export const getCouchDocs = () => aw(async (req, res, next) => {
-  console.log('req.cookies');
-  console.log(req.cookies);
+
   if (!(req.get('cookie') && req.get('cookie').substring(0, 8) === 'session=')) {
     return next();
   }
