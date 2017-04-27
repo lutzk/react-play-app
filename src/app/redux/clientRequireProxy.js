bf7070fed3188764e46867b73f9037c9ec3cc5bf
jonny <lutz.kwauka@gmail.com>
2017-04-27 22:50:01 +0200

    adjust selector of userDB in state

--- src/app/redux/clientRequireProxy.js
+++ src/app/redux/clientRequireProxy.js
@@ -7,7 +7,7 @@ const db = (reducerName, store) => {
   if (state.user.user && state.user.user.userId) {
     return {
       local: new PouchDB(`_localUser_${state.user.user.userId}` /* , { revs_limit: 20 } */),
-      remote: new PouchDB(state.user.user.userDBs.supertest),
+      remote: new PouchDB(state.user.user.userDB),
     };
   }
 
