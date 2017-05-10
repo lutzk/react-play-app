5f837e041e153fe8ff16b7bdfa2c15e67d737bae
jonny <lutz.kwauka@gmail.com>
2017-05-11 00:50:54 +0200

    just require state not getState()

--- src/app/redux/clientRequireProxy.js
+++ src/app/redux/clientRequireProxy.js
@@ -2,8 +2,7 @@ import PouchDB from 'pouchdb';
 import DevTools from '../containers/DevTools/DevTools';
 import { persistentStore } from '../../redux-pouchdb-plus/src/index';
 
-const db = (reducerName, store) => {
-  const state = store.getState();
+const db = (state) => {
   if (state.user.user && state.user.user.userId) {
     return {
       local: new PouchDB(`_localUser_${state.user.user.userId}` /* , { revs_limit: 20 } */),
