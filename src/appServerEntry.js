991836b00e6447716b13bc0ec0e391ad1348afd7
jonny <lutz.kwauka@gmail.com>
2017-05-14 18:09:38 +0200

    clean up

--- src/appServerEntry.js
+++ src/appServerEntry.js
@@ -1,23 +1,8 @@
 import { getJsonData } from './helpers/utils';
 import { startServer } from './appServer/startServer';
 
-global.__CLIENT__ = false;
-global.__SERVER__ = true;
-global.__DISABLE_SSR__ = false; // <----- DISABLES SERVER SIDE RENDERING FOR ERROR DEBUGGING
-global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 let appServer;
 
-if (global.__DEVELOPMENT__) {
-  (() => {
-    if (!require('piping')(
-      {
-        hook: true,
-        ignore: /(\/\.|~$|\.json$|\.\/node_modules\/$)/i,
-      }
-    )) {
-      // return;
-    }
-  })();
 const dev = __DEVELOPMENT__;
 
 if (dev && module && module.hot) {
@@ -25,7 +10,6 @@ if (dev && module && module.hot) {
   module.hot.accept(e => console.error('SERVER HMR ERROR:', e));
 }
 
-const dev = global.__DEVELOPMENT__;
 const options = {
   ...(dev ?
       { empty: true }
