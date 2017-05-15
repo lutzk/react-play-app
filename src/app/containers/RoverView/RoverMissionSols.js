55656bb66d1c58231f41ca9e3070988d46dfc0ef
jonny <lutz.kwauka@gmail.com>
2017-05-16 00:26:34 +0200

    back to simple

--- src/app/containers/RoverView/RoverMissionSols.js
+++ src/app/containers/RoverView/RoverMissionSols.js
@@ -18,40 +18,19 @@ const RoverMissionSols = (input) => {
     return <div className="listEmpty">no sols match</div>;
   }
 
-  // const handleClick = (e) => {
-  //   const sol = e.currentTarget.dataset.sol;
-
-  //   if (!sol) {
-  //     return;
-  //   }
-
-  //   input.push(`/sol/${sol}`);
-  // };
-
   const { rover, sols } = input;
 
   const solsCards = sols.map((sol, index) => (
-      // eslint-disable-next-line
-      <Link key={index} data-sol={sol.sol} className="solCard" to={`/${PATHS.ROVER_VIEW.ROOT}/${rover}/${PATHS.SOL}/${sol.sol}`}>
-        {Object.keys(sol).map((key, i) => {
-          if (typeof sol[key] === 'string' || typeof sol[key] === 'number') {
-            return (
-              <span key={i} className={`sol_card_${key}`}>{key}:&nbsp;{sol[key]}</span>);
-          }
-          if (typeof sol[key] === 'object' && key === 'cameras') {
-            return (
-              <span key={i} className={`sol_card_${key}`}>
-                cams:&nbsp;
-                {sol.cameras.map((cam, j) => (
-                  <span key={j}>{cam}&nbsp;</span>)
-                )}
-              </span>);
-          }
-          return 1;
-        })}
-      </Link>
-    )
- );
+    <Link key={index} data-sol={sol.sol} className="solCard" to={`/${PATHS.ROVER_VIEW.ROOT}/${rover}/${PATHS.SOL}/${sol.sol}`}>
+      <span className={'sol_card_sol'}>sol:&nbsp;{sol.sol}</span>;
+      <span className={'sol_card_sol'}>totalPhotos:&nbsp;{sol.totalPhotos}</span>;
+      <span className={'sol_card_cameras'}>
+        cams:&nbsp;
+        {sol.cameras.map((cam, j) => (
+          <span key={j}>{cam}&nbsp;</span>)
+        )}
+      </span>
+    </Link>));
 
   return (
     <div className="roverPhotoDataData">
