15dc4ae55234a7583b01f740f9d174dfa1f1a23e
jonny <lutz.kwauka@gmail.com>
2017-06-01 06:35:17 +0200

    cache service worker skelletton

---
+++ src/app/workers/cacheWorker.js
@@ -0,0 +1,17 @@
+
+
+const getWindowClients = () => self.clients.claim().then(() => self.clients.matchAll({ type: 'window' }));
+
+
+self.addEventListener('message', (event) => {
+  console.log('__MESSAGE_:', event.data.type, event);
+
+  event.ports[0].postMessage('ack');
+  // }
+});
+
+self.addEventListener('install', event =>
+  event.waitUntil(() => Promise.resolve(() => self.skipWaiting())));
+
+self.addEventListener('activate', event =>
+  event.waitUntil(() => Promise.resolve(() => self.clients.claim())));
