06fa05fcbbd4b168b2af06e660ccf96ac60725ff
jonny <lutz.kwauka@gmail.com>
2017-07-13 15:12:02 +0200

    play with reselect

---
+++ src/app/redux/selectors/userSelector.js
@@ -0,0 +1,41 @@
+import { createSelector } from 'reselect';
+
+const getUser = (state, props) => {
+  const userState = state.user;
+  // console.log('__USER__STATE', userState);
+  let user = {};
+  if (userState.user) {
+    user = {
+      id: userState.user.userId,
+      db: userState.user.userDB,
+    };
+  }
+  return user;
+};
+
+const getUserMeta = (state, props) => {
+  const userState = state.user;
+  // console.log('__USER__STATE', userState);
+  let userMeta = {};
+  if (userState) {
+    userMeta = {
+      savedPath: userState.savedPath,
+      loggedIn: userState.loggedIn,
+      loggingIn: userState.loggingIn,
+      lastLoaded: userState.lastLoaded,
+    };
+  }
+  return userMeta;
+};
+
+const makeGetUserState = () =>
+  createSelector(
+    [getUser],
+    user => user);
+
+const makeGetUserMeta = () =>
+  createSelector(
+    [getUserMeta],
+    userMeta => userMeta);
+
+export { makeGetUserState, makeGetUserMeta };
