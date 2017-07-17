018c22477c2364ca7a905f6a0e0813b664f7fe21
jonny <lutz.kwauka@gmail.com>
2017-07-17 19:31:52 +0200

    routes / path config

---
+++ src/app/redux/reduxRouterFirst/nav.js
@@ -0,0 +1,21 @@
+const HOME = 'HOME';
+const LOGIN = 'LOGIN';
+const SOL = 'SOL_VIEW';
+const ROVER_VIEW = 'ROVER_VIEW';
+
+const PATHS = {
+  // [ROOT]: '/',
+  [SOL]: '/sol',
+  [HOME]: '/home',
+  [LOGIN]: '/login',
+  [ROVER_VIEW]: '/rover-view',
+};
+
+
+export {
+  SOL,
+  HOME,
+  PATHS,
+  LOGIN,
+  ROVER_VIEW,
+};
--- src/app/redux/reduxRouterFirst/routesMap.js
+++ src/app/redux/reduxRouterFirst/routesMap.js
@@ -1,33 +1,28 @@
-import { redirect /* , NOT_FOUND */ } from 'redux-first-router';
-import { loadAuth, isLoaded, killUser } from '../modules/user';// eslint-disable-line
+// import { redirect /* , NOT_FOUND */ } from 'redux-first-router';
+import { /* loadAuth, isLoaded, killUser, */ requireLogin } from '../modules/user';// eslint-disable-line
+import { HOME, LOGIN, ROVER_VIEW, PATHS } from './nav';
 
-const requireLogin = (dispatch, getState) => {
-  const checkAuth = () => {
-    const { user: { user } } = getState();
-    if (!user) {
-      const action = redirect({ type: 'LOGIN', nextPathname: getState().location.pathname });// eslint-disable-line
-      dispatch(action);
-    }
-  };
-
-  if (!isLoaded(getState())) {
-    Promise.resolve(dispatch(loadAuth()))
-      .then(() => checkAuth());
-  } else {
-    checkAuth();
-  }
+const thunk = async (dispatch, getState) => {
+  await dispatch(requireLogin());
 };
 
 const routesMap = {
-  // LOGIN: '/',
-  LOGIN: '/login',
-  HOME: {
-    path: '/home',
-    thunk: requireLogin,
+  // [LOGIN]: '/',
+  [LOGIN]: {
+    path: PATHS[LOGIN],
+    thunk,
   },
-  ROVER_VIEW: {
-    path: '/rover-view/:rover',
-    thunk: requireLogin,
+  [HOME]: {
+    path: PATHS[HOME],
+    thunk,
+  },
+  [ROVER_VIEW]: {
+    path: `${PATHS[ROVER_VIEW]}/:rover`,
+    thunk,
+  },
+  SOL: {
+    path: `${PATHS[ROVER_VIEW]}/:rover/sol/:sol`,
+    thunk,
   },
 };
 
