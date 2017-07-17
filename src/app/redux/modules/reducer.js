a581c6ce8b2e70dd07ab01cc9960ee556ac2e7bc
jonny <lutz.kwauka@gmail.com>
2017-07-17 19:32:17 +0200

    clean up createRootReducer

--- src/app/redux/modules/reducer.js
+++ src/app/redux/modules/reducer.js
@@ -1,36 +1,22 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-import { reducer as reduxAsyncConnect } from 'redux-connect';
+// import { reducer as reduxAsyncConnect } from 'redux-connect';
 
 import { app } from './app';
 import { user } from './user';
+import { page } from './page';
+import { pageLoadBar } from './pageLoadBar';
 import { solViewReducer as solView } from './solView';
 import { roverViewReducer as roverView } from './roverView';
-import { pageLoadBar } from './pageLoadBar';
-import { page } from './page';
 
 const createRootReducer = location => combineReducers({
-  location,
+  app,
+  user,
   page,
-  // router: routerReducer,
-  // routing: routerReducer,
+  solView,
+  location,
+  roverView,
+  pageLoadBar,
   // reduxAsyncConnect,
-  app,
-  user,
-  solView,
-  roverView,
-  pageLoadBar,
 });
 
-const rootReducer = combineReducers({
-  router: routerReducer,
-  routing: routerReducer,
-  reduxAsyncConnect,
-  app,
-  user,
-  solView,
-  roverView,
-  pageLoadBar,
-});
-
-export { rootReducer, createRootReducer };
+export { createRootReducer };
