f3ee55991cb9b0ff867fbb2a6bb8bab1ef9485d3
jonny <lutz.kwauka@gmail.com>
2017-10-29 21:06:58 +0100

    add links to other rovers

--- src/app/containers/Home/Home.js
+++ src/app/containers/Home/Home.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Link from 'redux-first-router-link';
-import { linkToSpirit } from '../../redux/routing/navTypes';
+import { linkToSpirit, createRoverLink } from '../../redux/routing/navTypes';
 import '../Home/Home.sass';
 
 const Home = () => (
   <div className="page home">
     <h1>home</h1>
     <p>
-      <Link to={linkToSpirit}>RoverView (rover = "Spirit")</Link>
+      <Link to={linkToSpirit}>
+          RoverView (rover = "Spirit")
+      </Link>
+    </p>
+    <p>
+      <Link to={createRoverLink({ rover: 'curiosity' })}>
+          RoverView (rover = "Curiosity")
+      </Link>
+    </p>
+    <p>
+      <Link to={createRoverLink({ rover: 'opportunity' })}>
+          RoverView (rover = "Opportunity")
+      </Link>
     </p>
   </div>);
 
