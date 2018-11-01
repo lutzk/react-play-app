daf715f9eab03a67adfd5cb10ba7d736645690a4
jo <lutz.kwauka@gmail.com>
2018-11-01 12:06:32 +0100

    copy and patch original combineReducers tp inject user state WIP

--- src/app/redux/modules/reducer.ts
+++ src/app/redux/modules/reducer.ts
@@ -18,7 +18,7 @@ interface ApplicationState {
 }
 
 const createRootReducer = location =>
-  combineReducers<ApplicationState>({
+  _combineReducers({
     app,
     user,
     page,
@@ -27,5 +27,97 @@ const createRootReducer = location =>
     roverView,
     pageLoadBar,
   });
+
+const _combineReducers = reducers => {
+  const reducerKeys = Object.keys(reducers);
+  const finalReducers = {};
+  // tslint:disable-next-line:prefer-for-of
+  for (let i = 0; i < reducerKeys.length; i++) {
+    const key = reducerKeys[i];
+
+    if (process.env.NODE_ENV !== 'production') {
+      if (typeof reducers[key] === 'undefined') {
+        console.warn(`No reducer provided for key "${key}"`);
+      }
+    }
+
+    if (typeof reducers[key] === 'function') {
+      finalReducers[key] = reducers[key];
+    }
+  }
+  const finalReducerKeys = Object.keys(finalReducers);
+
+  // let unexpectedKeyCache;
+  // if (process.env.NODE_ENV !== 'production') {
+  //   unexpectedKeyCache = {};
+  // }
+
+  // let shapeAssertionError;
+  // try {
+  //   assertReducerShape(finalReducers);
+  // } catch (e) {
+  //   shapeAssertionError = e;
+  // }
+
+  return function combination(state: any = {}, action) {
+    // if (shapeAssertionError) {
+    //   throw shapeAssertionError;
+    // }
+
+    // if (process.env.NODE_ENV !== 'production') {
+    //   const warningMessage = getUnexpectedStateShapeWarningMessage(
+    //     state,
+    //     finalReducers,
+    //     action,
+    //     unexpectedKeyCache,
+    //   );
+    //   if (warningMessage) {
+    //     console.warn(warningMessage);
+    //   }
+    // }
+
+    let hasChanged = false;
+    const nextState = {};
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < finalReducerKeys.length; i++) {
+      const key = finalReducerKeys[i];
+      const reducer = finalReducers[key];
+      const previousStateForKey = state[key];
+      let nextStateForKey;
+      if (key === 'roverView' || key === 'solView') {
+        console.log('YEAHAHAHAHAH', key);
+        nextStateForKey = reducer(previousStateForKey, {
+          ...action,
+          user: state.user,
+        });
+      } else {
+        nextStateForKey = reducer(previousStateForKey, action);
+      }
+      if (typeof nextStateForKey === 'undefined') {
+        // const errorMessage = getUndefinedStateErrorMessage(key, action);
+        throw new Error('some key was undef');
+      }
+      nextState[key] = nextStateForKey;
+      hasChanged = hasChanged || nextStateForKey !== previousStateForKey;
+    }
+    return hasChanged ? nextState : state;
+  };
+};
+
+// const createRootReducer = location => (state: any = {}, action: any) => {
+//   const userState = state.user;
+//   return {
+//     app,
+//     user,
+//     page,
+//     solView: solView(state.solView, { ...userState }),
+//     location,
+//     roverView: roverView(state.roverView, { ...userState }),
+//     pageLoadBar,
+//     // languages: languages(state.languages, action),
+//     // // merge languageCodes with original action object, now you have access in translations reducer
+//     // translations: translations(state.translations, {...action, languageCodes})
+//   };
+// };
 
 export { createRootReducer };
