de628a0c70fb4ed22218f40a108a672909b7d626
jo <lutz.kwauka@gmail.com>
2018-11-18 23:21:29 +0100

    add image types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,8 +2,16 @@ declare var __CLIENT__: Boolean;
 declare var __SERVER__: boolean;
 declare var __DEVTOOLS__: boolean;
 declare var __DEVELOPMENT__: boolean;
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
 
 interface Window {
     __data: any;
     React: any;
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
 }
